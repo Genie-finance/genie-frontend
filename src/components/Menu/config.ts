9a8e5bc7d1189c73bef2bd66bb5cf072784bd1f0
Genie-finance <kolickovitalij@gmail.com>
2021-04-09 02:24:28 +0300

    a

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,84 @@
+import { MenuEntry } from '@pancakeswap-libs/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: 'Exchange',
+        href: 'https://exchange.geniedefifinance.com/',
+      },
+      {
+        label: 'Liquidity',
+        href: 'https://exchange.geniedefifinance.com/#/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: 'Nests',
+    icon: 'PoolIcon',
+    href: '/nests',
+  },
+  // {
+  //   label: 'Pools',
+  //   icon: 'PoolIcon',
+  //   href: '/pools',
+  // },
+  // {
+  //   label: 'Lottery',
+  //   icon: 'TicketIcon',
+  //   href: '/lottery',
+  // },
+  // {
+  //   label: 'NFT',
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  {
+    label: 'Info',
+    icon: 'InfoIcon',
+    items: [
+      {
+        label: 'PancakeSwap',
+        href: 'https://pancakeswap.info/token/0xF952Fc3ca7325Cc27D15885d37117676d25BfdA6',
+      }
+    ],
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/Genie-finance',
+      },
+      {
+        label: 'Docs',
+        href: 'https://app.gitbook.com/@geniefinance/s/genie-finance/',
+      }
+    ],
+  },
+  {
+    label: 'Lottery(Comming soon)',
+    icon: 'TicketIcon',
+    href: '',
+  },
+  {
+    label: 'Audit by Hacken(Comming soon)',
+    icon: 'AuditIcon',
+    href: '',
+  },
+]
+
+export default config
---
+++ src/config/constants/contracts.ts
@@ -0,0 +1,30 @@
+export default {
+  cake: {
+    56: '0xB192cF5317CC88eb3Db88eA59003177aD62F189F',
+    97: '',
+  },
+  masterChef: {
+    56: '0x692eCf37570A878EB4bEC76EDe59CF00255AF50F',
+    97: '',
+  },
+  wbnb: {
+    56: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c',
+    97: '',
+  },
+  lottery: {
+    56: '',
+    97: '',
+  },
+  lotteryNFT: {
+    56: '',
+    97: '',
+  },
+  mulltiCall: {
+    56: '0x1ee38d535d541c55c9dae27b12edf090c608e6fb',
+    97: '0x67ADCB4dF3931b0C5Da724058ADC2174a9844412',
+  },
+  busd: {
+    56: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
+    97: '',
+  },
+}
